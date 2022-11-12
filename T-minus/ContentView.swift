405622d6b4cd09d132d61e1c810d38d804e9a452
Oliver Stenning <oliver@stenning.co>
2022-11-12 21:26:08 +0000

    Initial Commit

---
+++ T-minus/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  T-minus
+//
+//  Created by Oliver Stenning on 12/11/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ T-minus/T_minusApp.swift
@@ -0,0 +1,17 @@
+//
+//  T_minusApp.swift
+//  T-minus
+//
+//  Created by Oliver Stenning on 12/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct T_minusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
